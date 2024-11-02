20f7d044b758022cf9db4e9c392b8364827b9f5a
seunghyeokleeme <seunghyeok.tech@gmail.com>
2024-11-02 21:38:20 +0900

    Animations and Pseudo Selectors

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import styled, { keyframes } from "styled-components";
+
+const Wrapper = styled.div`
+  display: flex;
+`;
+
+const roatationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 50px;
+  }
+  100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${roatationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 40px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
+`;
+
+function App() {
+  return (
+    <Wrapper>
+      <Box>
+        <span>😂</span>
+      </Box>
+    </Wrapper>
+  );
+}
+
+export default App;
